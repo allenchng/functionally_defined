211438d785e1d3c5e9dbcc1f92e0959ed7f268c7
Allen Chang <achang35@gmail.com>
2018-01-24 09:03:54 -0500

    add slp data

--- data/slp.R
+++ data/slp.R
@@ -4,11 +4,12 @@ library(stringr)
 library(tm)
 library(tidytext)
 library(broom)
+library(lubridate)
 
 theme_set(theme_bw())
 
 url <- "https://www.styleforum.net/threads/saint-laurent-paris-official-thread.317702/page-"
-page <- c(2:13)
+page <- c(665:1330)
 #1329
 site <- paste0(url, page)
 
@@ -50,9 +51,90 @@ clean_corp <- clean_corpus(content_corpus)
 content_dtm <- DocumentTermMatrix(clean_corp)
 
 content_td <- tidy(content_dtm)
+# saveRDS(content_td, "slp1_td.rds")
+# saveRDS(content_td, "slp2_td.rds")
+slp1 <- readRDS("slp1_td.rds")
+slp2 <- readRDS("slp2_td.rds")
 
+full_df <- bind_rows(slp1, slp2)
+#### SOMETHING IS STILL WEIRD HERE
+full_df$document <- mdy(full_df$document)
 
-test <- as.data.frame(as.matrix(content_dtm))
+### BING
+
+post_sent_bing <- full_df %>%
+  inner_join(get_sentiments("bing"), by = c(term = "word"))
+
+post_sent_bing %>%
+  count(sentiment, term, wt = count) %>%
+  ungroup() %>%
+  filter(n >= 200) %>%
+  mutate(n = ifelse(sentiment == "negative", -n, n)) %>%
+  mutate(term = reorder(term, n)) %>%
+  ggplot(aes(term, n, fill = sentiment)) +
+  geom_bar(stat = "identity") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  ylab("Contribution to sentiment")
+
+post_sent_bing %>%
+  count(term, sort = TRUE) %>%
+  head(10)
+
+### NRC
+post_sent_nrc <- full_df %>%
+  inner_join(get_sentiments("nrc"), by = c(term = "word"))
+
+post_sent_nrc %>%
+  # Filter to only choose the words associated with sadness
+  filter(sentiment == "sadness") %>%
+  # Group by word
+  group_by(term) %>%
+  # Use the summarize verb to find the mean frequency
+  summarize(count = mean(count)) %>%
+  # Arrange to sort in order of descending frequency
+  arrange(desc(count))
+
+joy_words <- post_sent_nrc %>%
+  # Filter to only choose the words associated with sadness
+  filter(sentiment == "joy") %>%
+  # Group by word
+  group_by(term) %>%
+  # Use the summarize verb to find the mean frequency
+  summarize(count = mean(count)) %>%
+  # Arrange to sort in order of descending frequency
+  arrange(desc(count))    
+
+joy_words %>%
+  top_n(20) %>%
+  mutate(word = reorder(term, count)) %>%
+  # Use aes() to put words on the x-axis and frequency on the y-axis
+  ggplot(aes(word, count)) +
+  # Make a bar chart with geom_col()
+  geom_col() +
+  coord_flip()
+
+sentiment_by_time <- full_df %>%
+  # Define a new column using floor_date()
+  mutate(date = floor_date(show_date, unit = "6 months")) %>%
+  # Group by date
+  group_by(date) %>%
+  mutate(total_words = n()) %>%
+  ungroup() %>%
+  # Implement sentiment analysis using the NRC lexicon
+  inner_join(get_sentiments("nrc"))
+
+### afinn
+post_sent_afinn <- full_df %>%
+  inner_join(get_sentiments("afinn"), by = c(term = "word"))
+
+sentiment_contributions <- full_df %>%
+  # Count by title and word
+  count(term, sort = TRUE) %>%
+  # Implement sentiment analysis using the "afinn" lexicon
+  inner_join(get_sentiments("afinn"), by = c(term = "word")) %>%
+  # Calculate a contribution for each word in each title
+  mutate(contribution = score * n / sum(n)) %>%
+  ungroup()
 
 ### REFERENCES
 
